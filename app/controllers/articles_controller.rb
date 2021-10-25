ff4acaa78a7f8498c85c0c8b612e1d9522aa49f4
amit <ashethe@deqode.com>
2021-10-25 18:20:45 +0530

    callback implemented

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,52 +1,41 @@
 class ArticlesController < ApplicationController
+before_action :intialize_Article_object, only: %i[show edit update destroy]
+
   def index
     @article = Article.all
   end
-  
-  def show
     
-     @article = Article.find(params[:id])
-      
-  
-    
-    #@article = Article.find_by(title: params[:title])
-  end
-  
   def new
     @article = Article.new 
   end
   
   def create
     @article = Article.new(article_params)
-
     if @article.save
       redirect_to @article
     else
       render :new
     end
   end
-  def edit
-
-    @article = Article.find(params[:id])
-
-  end
-  def update
-    @article = Article.find(params[:id])
+  
+  def update    
     if @article.update(article_params)
       redirect_to article_path
       else
         render :edit
     end
   end
-  def destroy
-    @article = Article.find(params[:id])
+  def destroy    
     @article.destroy
     redirect_to root_path
   end
 
   private
-
   def article_params
     params.require(:article).permit(:title,:body,:email,:email_confirmation)
+  end
+
+  def intialize_Article_object
+    @article = Article.find(params[:id])
   end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,22 +5,18 @@ class CommentsController < ApplicationController
         @comment = Comment.all
     end
 
-    def create
-        
-        @comment = @article.comments.create(comment_params)
-        
+    def create        
+        @comment = @article.comments.create(comment_params)        
         if @comment
             redirect_to article_comments_path
-        else
-            byebug
+        else            
             render :error
         end
     end
 
     private
     def comment_params
-        params.require(:comment).permit(:commenter,:body)
-        #redirect_to article_path(@article)
+        params.require(:comment).permit(:commenter,:body)        
     end
 
     def find_article
